27b14bb0ed21fb31c8efb4c5ddb551ab6714c051
T0Mage <tompraszkiewicz@gmail.com>
2020-10-30 12:40:27 +0100

    initial

--- task.py
+++ task.py
@@ -1,30 +1,34 @@
-# Class diary
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-#
-# Please, use your imagination and create more functionalities.
-# Your project should be able to handle entire school(s?).
-# If you have enough courage and time, try storing (reading/writing)
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface (might be text-like).
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+class Student:
+    def __init__(self,fname="unnknown",lname="unnknown"):
+        self.fname,self.lname=fname,lname
+        self.grades={}
+        self.attendance={}
+
+    def add_subject(self,subjectName):
+        self.grades[subjectName]=[]
+        self.attendance[subjectName]=[]
+
+    def add_grade(self,subjectName,grade):
+        self.grades[subjectName].append(grade)
+
+    def average_grade(self,subjectName):
+        return sum(self.grades[subjectName])/len(self.grades[subjectName])
+
+
+    def add_attendance(self,subjectName,isPresent=False):
+        self.attendance[subjectName].append({"isPresent":isPresent})
+
+    def __repr__(self):
+        return f"{self.fname},{self.lname},{self.grades},{self.attendance}"
+    
+if __name__ == "__main__":
+    testStudent= Student("john","xyz")
+    testStudent.add_subject("math")
+    testStudent.add_attendance("math",True)
+    testStudent.add_grade("math",3)
+    testStudent.add_grade("math",4)
+    testStudent.add_attendance("math")
+    print("avg grade:",testStudent.average_grade("math"))
+
+    print(testStudent)
